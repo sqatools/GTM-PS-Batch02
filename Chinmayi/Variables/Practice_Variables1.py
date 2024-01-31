7c9b3d92887bd628b77f3b109c24f3d4f189cf7c
Chinmayi92 <chinmayinayak92@gmail.com>
2024-01-31 15:47:50 +0530

    practice the variables

---
+++ Chinmayi/Variables/Practice_Variables1.py
@@ -0,0 +1,76 @@
+#1. Python Program to add two integer values
+x = 50
+y = 60
+
+print("Add =",(x+y))
+
+#2. Python Program to subtract two integer values
+
+x = 1000
+y = 670
+
+print("Sub =",(x-y))
+
+#3.Python program to multiply two numbers
+
+X = 25
+y = 4
+
+print("Mul =",(X*y))
+
+#4.Python program to repeat a given string 5 times.
+
+str1 = "SQATools"
+
+print(str1*5)
+
+#5.Python program to get the Average of given numbers
+
+a = 40
+b = 50
+c = 30
+
+print("Average =",(a+b+c)//3)
+
+#6.Python program to print the square and cube of a given number.
+num1 = 9
+
+print("Square =",num1**2)
+print("Cube =",num1**3)
+
+#7. Python program to interchange values between variables.
+
+a = 10
+b = 20
+
+a,b = b,a
+print("a =",a)
+print("b =",b)
+
+#8.Python program to solve this Pythagorous theorem
+#(a + b)2 = a^2 + b^2 + 2ab
+
+a = 5
+b = 7
+
+print("(a+b)2 =",a**2+b**2+2*a*b)
+
+#9.Python program to solve the given math formula
+#(a – b)2 = a^2 + b^2 – 2ab
+
+a = 20
+b = 4
+
+print("(a-b)2 =",a**2+b**2-2*a*b)
+
+#10.Python program to solve the given math formula
+#(a + b)3 = a3 + 3ab(a+b) + b3
+
+a = 5
+b = 6
+c = a**3+3*a*b*(a+b)+b**3
+
+print("(a+b)3=",c)
+
+
+
