6bc760406f82cf2f854e0671a6f908ac5ee1573f
Chinmayi92 <chinmayinayak92@gmail.com>
2024-01-31 13:29:04 +0530

    Variable Practice

--- Chinmayi/Variables/Program1.py
+++ Chinmayi/Variables/Program1.py
@@ -1,7 +1,42 @@
-                                            Var1 = 30
-
+# address of the variable
+Var1 = 30
 print(id(Var1))
 
-# power operator
-x = 4
-print("value of x",x**4)
+
+# Variable name is Case Sensitive
+
+name = "Rahul"
+Name = "Ravi"
+NAME = "RAHUL"
+Name1 = "Mohit"
+NamE = "SOBIT"
+
+print(name, Name, NAME, Name1,NamE)
+
+#Assigned mutliple values to multiple variables
+
+p, q,r,t,u = 20,30,40,40,50
+print("Value of p:",p)
+print("Value of q:",q)
+print("Value of r:",r)
+print("Value of t:",t)
+print("Value of u:",u)
+
+#Assigned single value to multiple variables
+
+x=y=z=i = 60
+
+print("Value of variables:",x,y,z,i)
+print("Value of x",x)
+print("Value of y",y)
+print("Value of z",z)
+print("Value of i",i)
+
+#Strings
+
+Str_1 = "Good Morning"
+print(Str_1)
+
+print(Str_1*4)
+
+
