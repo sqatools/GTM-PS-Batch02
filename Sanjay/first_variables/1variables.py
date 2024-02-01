9b489006710fbd3c138790620f5b29fa1f786df3
sanjaybirajdar0990 <sanjaybirajdar0990@gmail.com>
2024-02-01 07:20:35 +0530

    added new variables1

---
+++ Sanjay/first_variables/1variables.py
@@ -0,0 +1,58 @@
+# Variables
+a = 10
+b = 20
+print(id(a))
+print(id(b))
+c = 10
+print(id(c))
+
+average = a+b+c/3
+print("average : ", average)
+
+
+var33 = 33
+#Case sensitive language
+name = "rahul"
+Name = "soham"
+NAME = "akash"
+
+print(name)
+print(Name)
+print(NAME)
+
+#Assigning multiple values to multiple variables
+a, b, c = 70, 80, 90
+print("value of a :", a)
+print("value of b :", b)
+print("value of c :", c)
+
+#Assign single value to multiple variable
+x = y = z = 120
+print("value of x :", x)
+print("value of y :", y)
+print("value of z :", z)
+
+#Addition
+var1 = 800
+var2 = 300
+add1 = var1 + var2
+print("addition of add1 :", add1)
+
+#Substraction
+sub1 = var1 - var2
+print("substraction of sub1 : ", sub1)
+
+#Multiplication
+mul1 = var1 * var2
+print("multiplication of mul1: ", mul1)
+
+#Division using single /
+div1 = var1 / var2
+print("Division of div1 :", div1)
+
+#Division using double //
+div2 = var1 // var2
+print("Division of div2 :", div2)
+
+str1 = " sqatools"
+print(str1 * 5)
