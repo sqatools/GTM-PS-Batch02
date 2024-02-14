e57071d797159753ccbfe75747e9c4ab7f47b36f
sowrabhaQA <sowrihr123@gmail.com>
2024-02-14 22:15:35 +0530

    if else programs

---
+++ Sowrabha/If else programs assignment/No divided by 3 and 5.py
@@ -0,0 +1,7 @@
+#Python program to check the given number divided by 3 and 5.
+
+Number=int(input("enter number"))
+if Number%3==0 and Number%5==0:
+    print("yes both are divisable")
+else:
+    print("both are not divisable")
