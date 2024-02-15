67bd0b46a9b224f2c371fb095db57f44ee02854b
nehakumari270122 <nehakumari.ece15@gmail.com>
2024-02-15 17:49:30 +0530

    Merge remote-tracking branch 'origin/main'

---
+++ Neha/Loop_programs/Check_even&odd.py
@@ -0,0 +1,7 @@
+#python for even or odd:
+
+for i in range(1,10):
+    if i%2==0:
+        print (i,"is even no")
+    else:
+        print(i,"odd no")
---
+++ Neha/Loop_programs/Prime.py
@@ -0,0 +1,12 @@
+##Python program to check given number is a prime number or not
+num = int(input("enter the input number:"))
+count = 0
+
+if num>1:
+    for i in range(1,num+1):
+        if (num%i)==0:
+            count=count+1
+    if count==2:
+        print("prime")
+    else:
+        print("not prime")
