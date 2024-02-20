02725701fba1429127a09195a6df8acd0e9aa7da
Chinmayi92 <chinmayinayak92@gmail.com>
2024-02-20 18:14:43 +0530

    For loop practice

--- Chinmayi/Loop_Practice/For_Loop_Practice.py
+++ Chinmayi/Loop_Practice/For_Loop_Practice.py
@@ -1,41 +1,48 @@
 #range(initial value, end value, different value)
 #it will print the value till end_value - 1
-for i in range(5, 10, 1):
+for i in range(1, 10, 1):
     print(i)
-"""
+print("*"*70)
 # range with two parameter
 # range(initial value, end value)
 # default difference value is 1 and exclude last value.
 
-for i in range(1, 10):
+for i in range(1, 11):
     print(i)
+print("*"*70)
 
 # range with one parameter : range(last value)
 # default initial value will 0 and difference value will be 1
-print("_"*50)
+
 for j in range(5):
     print(j)
     print("*" * 5)
 
+print("*"*70)
+
 # write a program to print the table of any given number
-print("_"*40)
+
 num1 = 6
 for i in range(1, 11):
-    print(i, "*", num1, ":", i*num1)
+    print(num1, "*", i, "=", i*num1)
 
+print("*"*70)
 
 # print values in reverse order
-print("_"*50)
+
 for i in range(10, 0, -1):
     print(i)
 
+print("*"*70)
+
 # check if condition in loop
 # get all the even number from 1 to 50
-print("_"*50)
+
 for i in range(1, 50):
     if i%2 == 0:
         print(i)
 
+print("*"*70)
 
 # get factorial of any given number
 # factorial of 5 : 5*4*3*2*1 = 120
@@ -53,7 +60,8 @@ for i in range(num, 0, -1):
 print("Factorial output :", fact)
 
 
-print("_"*50)
+print("*"*70)
+
 ### Continue and break condition
 
 for i in range(10):
@@ -69,4 +77,3 @@ for i in range(10):
         break
     print(i)
 
-"""
