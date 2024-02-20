bb77bec5b6e56cb8aba380b7966e2f1d0317dbb6
nehakumari270122 <nehakumari.ece15@gmail.com>
2024-02-20 17:53:50 +0530

    Merge remote-tracking branch 'origin/main'

---
+++ Neha/Assignment_folder/Homework.py
@@ -0,0 +1,54 @@
+# Slicing pratice programs
+
+"""
+write python program get following output
+
+str1= "Hello Good Morning"
+output = "gello Good MorninH"
+
+str2 = "Python Programming"
+output2 = "Pto rgamn"
+
+
+str3 = "Learning Python"
+output = "gninraeL nohtyP"
+
+str4 = "Good Evening"
+output4 = "GGood Eveningg"
+"""
+
+print("__"*50)
+
+input_str = "Hello Good Morning"
+output_str = input_str[0].replace('H', 'g') + input_str[1:] + input_str[-1].replace('g', 'H')
+
+print("Modified output:", output_str)
+
+print("__"*50)
+
+str2 = "Python Programming"
+#output= "Pto rgamn"
+output_str2=str2[::2]
+print(output_str2) #Pto rgamn
+
+print("__"*50)
+
+str3 = "Learning Python"
+#output = "gninraeL nohtyP"
+word= str3[-1: -18: -1] # nohtyP gninraeL
+print(word)
+x = word.split()
+print(x) #['nohtyP', 'gninraeL']
+y = x[-1::-1]
+print(y) #['gninraeL', 'nohtyP']
+reverse_str = ' '.join(y)
+print(reverse_str) #gninraeL nohtyP
+
+print("__"*50)
+
+input_str = "Good Evening"
+#output4 = "GGood Eveningg"
+output_str = input_str[0] + input_str[0] + input_str[1:] + input_str[-1]
+print("Modified output:", output_str)
+
+
