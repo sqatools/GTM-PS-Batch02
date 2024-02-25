c7e612cbcefcb1ddde3fc58e68e11f6b57057854
PeddiBharath <bharath.viru2@gmail.com>
2024-02-25 23:49:50 +0530

    ok

---
+++ Bharath/Assigment4 if_else/Given string is palindrome or not..py
@@ -0,0 +1,6 @@
+s1=input("Enter number:")
+rev=s1[::-1]
+if s1==rev:
+    print("Entered string Palidrome")
+else:
+    print("Entered string not a Palidrome")
---
+++ Bharath/Assigment4 if_else/Student has passed the exam.py
@@ -0,0 +1,6 @@
+m=int(input("Enter number:"))
+
+if m>35:
+        print("Student had passed exam")
+else:
+        print("student not passed exam")
