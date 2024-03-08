aef753f7ac1185aa615b73af21a11447f48268e4
pkiran46 <phanikiran.jbs@gmail.com>
2024-03-09 02:14:01 +0530

    sample 5 file upload

---
+++ Phanikiran/Filehandling/pythonfilehandling1.py
@@ -0,0 +1,9 @@
+def read_file(filename):
+     file = open(filename, "r")
+     data = file.read()
+     print(data)
+
+
+#read_file("test_data.txt")
+#read_file(r"E:\Filesdata\count_name.txt")
+read_file(r"C:\Users\pkira\Downloads\sample1.txt")
