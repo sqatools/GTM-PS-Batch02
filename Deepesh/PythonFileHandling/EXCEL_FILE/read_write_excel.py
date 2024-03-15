6491619ed296be3e7dd5788c2f5475ae86e2d381
Bi12Python <xovek11875@bitvoo.com>
2024-03-15 20:54:35 +0530

    Read write Excel and Json file

--- Deepesh/PythonFileHandling/EXCEL_FILE/read_write_excel.py
+++ Deepesh/PythonFileHandling/EXCEL_FILE/read_write_excel.py
@@ -29,10 +29,11 @@ def read_excel_data_method2(filename, sheet_name, cell_name):
 
 #read_excel_data_method2("read_excel_data.xlsx", sheet_name='Sheet1', cell_name="B5")
 
-for i in range(1, 6):
-    read_excel_data_method2("read_excel_data.xlsx", sheet_name='Sheet1', cell_name=f"B{i}")
+# for i in range(1, 6):
+#     read_excel_data_method2("read_excel_data.xlsx", sheet_name='Sheet1', cell_name=f"B{i}")
 
 
+# Create new excel file and add content
 def write_content_to_file(filename, sheet_name, cell_name, cell_value):
     wb = openpyxl.Workbook()
     sheet_obj = wb.active
@@ -41,7 +42,24 @@ def write_content_to_file(filename, sheet_name, cell_name, cell_value):
     wb.save(filename)
 
 
-write_content_to_file("read_excel_data.xlsx", "Sheet1", "A1", "Sanjay")
+write_content_to_file("read_excel_data3.xlsx", "Sheet1", "A1", "Sanjay")
+
+# add content to existing excel file
+def write_content_to_file_without_overwrite(filename, sheet_name, cell_name, cell_value):
+    wb = openpyxl.load_workbook(filename)
+    sheet_obj = wb[sheet_name]
+    cell = sheet_obj[f'{cell_name}']
+    cell.value = cell_value
+    wb.save(filename)
+
+#write_content_to_file_without_overwrite("read_excel_data.xlsx", "Sheet2", "A2", "Modi Ji")
+
+# for i in range(1, 10):
+#     write_content_to_file_without_overwrite("read_excel_data.xlsx", "Sheet2", f"A{i}", f"{535534+i}")
+
+
+
+
 
 
 
