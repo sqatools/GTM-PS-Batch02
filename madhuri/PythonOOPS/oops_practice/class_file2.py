8d61b206be062541add8ce0a1125959d38b667f4
madhurirahane <madhuri.rahane66@gmail.com>
2024-03-26 00:01:18 +0530

    Python OOPS Inheritance Porogram Practice : DONE

--- madhuri/PythonOOPS/oops_practice/class_file2.py
+++ madhuri/PythonOOPS/oops_practice/class_file2.py
@@ -1,9 +1,86 @@
 from class_file1 import Admin
-class Account(Admin):
-    def __init__(self, acc_name, admin_name):
-        super().__init__(admin_name)
-        self.acc_name = acc_name
+import json
+import os
 
 
-    def show_acc_name(self):
-        print("Account name :", self.acc_name)
+class School(Admin):
+    def __init__(self):
+        super().__init__()
+        print("School Class: ")
+
+    def add_school(self, add_new=None):
+        """
+        :return:true if data added /updated sucessfully
+        :param: school_name, school_address, school_phone, add_new
+        """
+
+        print("----- Enter School Details ------- ")
+        school_name = str(input("Enter School Name: "))
+        school_address = str(input("Enter School Address: "))
+        school_phone = int(input("Enter School Phone: "))
+
+        sc_dict = {
+            'name': school_name,
+            'address': school_address,
+            'phone': school_phone,
+            'admin_id': 1
+        }
+
+        with open('school_data.json', 'r') as file:
+            file_data = file.read()
+            if not file_data:
+                data = {
+                    'school':
+                        [
+                            sc_dict
+                        ]
+                }
+            else:
+                data = json.loads(file_data)
+                if data.get('school') is not None:
+                    school_list = data.get('school')
+                    school_list.append(sc_dict)
+                    data['school'] = school_list
+                else:
+                    data['school'] = [sc_dict]
+            self.__write_school_data_file(data)
+            self.show_school()
+
+    def __write_school_data_file(self, school_dict):
+        try:
+
+            with open('school_data.json', 'w') as file:
+                print(school_dict)
+                json_data = json.dumps(school_dict)
+                file.write(json_data)
+        except Exception as e:
+            print("Failed to write into file school_data.json: ", e)
+
+    def show_school(self):
+        print('_' * 20, 'School Details', '_' * 20)
+        filepath = 'school_data.json'
+
+        try:
+            filesize = os.path.getsize(filepath)
+            if filesize == 0:
+                print("----- No School Found ------")
+                add_new = int(input("Do you want to add new school 1/0? : "))
+                if add_new == 1:
+                    self.add_school(add_new=1)
+                else:
+                    print("----- Thank You -------")
+            else:
+                with open('school_data.json', 'r') as file:
+                    file_data = file.read()
+                    data = json.loads(file_data)
+                    if data.get('school') is not None:
+                        school_list = data.get('school')
+                        print("-"*50, "School Details", "-"*50)
+                        for val in school_list:
+                            print("Name: ", val['name'])
+                            print("Address: ", val['address'])
+                            print("Phone: ", val['phone'])
+                            print("_"*20)
+
+        except FileNotFoundError as e:
+            print("File not found", e)
