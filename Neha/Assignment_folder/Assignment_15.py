8ef8ed8c7c137ef39f8a17e1531ba505576c3b01
nehakumari270122 <nehakumari.ece15@gmail.com>
2024-03-29 20:13:36 +0530

    Code practice

---
+++ Neha/Assignment_folder/Assignment_15.py
@@ -0,0 +1,45 @@
+"""
+1). Python function program to add two numbers.
+
+Solution
+2). Python function program to print the input string 10 times.
+
+Solution
+3). Python function program to print a table of a given number.
+
+Solution
+4). Python function program to find the maximum of three numbers.
+
+Input: 17, 21, -9
+Output: 21
+
+Solution
+5). Python function program to find the sum of all the numbers in a list.
+Input : [6,9,4,5,3]
+Output: 27
+
+Solution
+6). Python function program to multiply all the numbers in a list.
+Input : [-8, 6, 1, 9, 2]
+Output: -864
+
+Solution
+7). Python function program to reverse a string.
+Input: Python1234
+Output: 4321nohtyp
+
+Solution
+8). Python function program to check whether a number is in a given range.
+Input : num = 7, range = 2 to 20
+Output: 7 is in the range
+
+Solution
+9). Python function program that takes a list and returns a new list with unique elements of the first list.
+Input : [2, 2, 3, 1, 4, 4, 4, 4, 4, 6]
+Output : [2, 3, 1, 4, 6 ]
+
+Solution
+10). Python function program that take a number as a parameter and checks whether the number is prime or not.
+Input : 7
+Output : True
+"""
