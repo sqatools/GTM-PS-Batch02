e25e21fc3a04fe5f390af3308eb36665450fbec6
Mathematics13 <haripriyakrishnan4@gmail.com>
2024-04-02 16:56:02 +0100

    Class

---
+++ MathHP/SeleniumCode/SeleniumFundamentals/first_selenium_script.py
@@ -0,0 +1,23 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()                   # Launch empty browser
+# driver=webdriver.Firefox()
+# driver=webdriver.Edge()                    # Multi browser launching
+
+driver.get("https://www.google.co.in/")
+driver.find_element(By.ID,"L2AGLb").click()
+
+driver.maximize_window()
+driver.implicitly_wait(10)
+
+driver.find_element(By.ID,"APjFqb").send_keys("Python Selenium")
+time.sleep(5)
+
+driver.find_element(By.NAME,"btnK").click()
+time.sleep(10)
+#driver.close()
+
+
