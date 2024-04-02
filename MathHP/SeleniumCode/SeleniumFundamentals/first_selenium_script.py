6c5e9922fbfd81f0fa96b5fd2291e8908bb61162
Mathematics13 <haripriyakrishnan4@gmail.com>
2024-04-02 17:03:43 +0100

    Class

--- MathHP/SeleniumCode/SeleniumFundamentals/first_selenium_script.py
+++ MathHP/SeleniumCode/SeleniumFundamentals/first_selenium_script.py
@@ -8,16 +8,17 @@ driver = webdriver.Chrome()                   # Launch empty browser
 # driver=webdriver.Edge()                    # Multi browser launching
 
 driver.get("https://www.google.co.in/")
-driver.find_element(By.ID,"L2AGLb").click()
+driver.find_element(By.ID,"L2AGLb").click()           # accept cookies
 
 driver.maximize_window()
 driver.implicitly_wait(10)
 
-driver.find_element(By.ID,"APjFqb").send_keys("Python Selenium")
+driver.find_element(By.ID,"APjFqb").send_keys("Python Selenium")        # search
 time.sleep(5)
 
-driver.find_element(By.NAME,"btnK").click()
+driver.find_element(By.NAME,"btnK").click()                               # google search
 time.sleep(10)
+
 #driver.close()
 
 
