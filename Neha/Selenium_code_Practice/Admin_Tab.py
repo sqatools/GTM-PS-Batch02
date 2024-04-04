c1b2fe3e0142cead3d24faf97e52e3bb68fd42f0
nehakumari <nehakumari270122@gmail.com>
2024-04-04 16:22:52 +0530

    code merge

---
+++ Neha/Selenium_code_Practice/Admin_Tab.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver.find_ele
+
+//div//label[contains(text(), 'Username')]//ancestor::div[@id = 'app']//preceding::input[@class = 'oxd-input oxd-input--active'][2]
+
