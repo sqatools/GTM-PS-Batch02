55b0de01a0a02c3ba346d4cf772f8f54adfcadd0
nehakumari <nehakumari270122@gmail.com>
2024-04-05 15:43:41 +0530

    code merge

--- Neha/Loop_programs/Dictionary.py
+++ Neha/Loop_programs/Dictionary.py
@@ -11,3 +11,22 @@ output_dict={}
 for i in range (len(list2)):
     output_dict[(list2[i])*2] = list1[i]
 print(output_dict)
+
+
+#String into dictionary
+car = "'make':'BMW','model':'1100','Year':'2010'"
+# output ={ 'make':'BMW','model':'1100','Year':'2010' }
+
+car_dict = {}
+
+# Split the string by comma and iterate over each key-value pair
+for pair in car.split(","):
+    # Split each pair by colon
+    key, value = pair.split(":")
+    # Remove extra spaces and single quotes
+    key = key.strip().strip("'")
+    value = value.strip().strip("'")
+    # Add key-value pair to the dictionary
+    car_dict[key] = value
+
+print(car_dict)
