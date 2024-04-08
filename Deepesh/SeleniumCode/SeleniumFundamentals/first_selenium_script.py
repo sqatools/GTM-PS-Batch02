0fb8f99116c655ce7b68908e1f1f3aba6f7764b6
swamini4321 <swaminikumthekar77@gmail.com>
2024-04-08 18:30:23 +0530

    selenium code

--- Deepesh/SeleniumCode/SeleniumFundamentals/first_selenium_script.py
+++ Deepesh/SeleniumCode/SeleniumFundamentals/first_selenium_script.py
@@ -1,4 +1,4 @@
-import time
+"""import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
@@ -12,5 +12,22 @@ driver.maximize_window()
 driver.implicitly_wait(10)
 driver.find_element(By.NAME, "q").send_keys("Python Selenium")
 driver.find_element(By.NAME, "btnK").click()
+time.sleep(5)
+driver.close()"""
+
+
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+driver = webdriver.Chrome()
+
+driver.get("https://automationbysqatools.blogspot.com/2021/05/dummy-website.html")
+driver.maximize_window()
+driver.implicitly_wait(10)
+
+driver.find_element(By.XPATH, "//div[@id='billing_details']//following-sibling::input[@ type='checkbox']").click()
+
 time.sleep(5)
 driver.close()
---
+++ Swamini/selenium assigments/selenium_testcode.py
@@ -0,0 +1,32 @@
+import time
+
+from selenium_common1 import *
+from selenium_locator import *
+from testdata import *
+
+get_driver(website_url, "Chrome", timeout=30)
+#click_element(Ticket_locator)
+send_data('rahul', first_name_locator)
+send_data('Gupta', last_name_locator)
+send_data(B_name, Billingname_locator)
+send_data(B_phone,phone_locator)
+send_data(B_email,email_locator)
+send_data(B_address,street_locator)
+send_data(B_postcode,post_locator)
+send_data(B_Prefecture,prefecture_locator)
+send_data(S1_address,S_address_locator)
+send_data(S2_address,S1_address_locator)
+click_element(place_locator)
+time.sleep(10)
+#send_data(Birthday,DOB_locator)
+#send_data('06/06/1996',DOB_locator)
+click_element(male_locator)
+click_element(female_locator)
+click_element(Trip_locator)
+click_element(Msg_locator)
+send_data(source_city_name, fromcity_locator)
+send_data(dest_city_name, destination_locator)
+time.sleep(5)
+
+
+
