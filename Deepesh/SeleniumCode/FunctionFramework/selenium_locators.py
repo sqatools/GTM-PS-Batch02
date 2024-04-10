43ac16f06601c2e9e999cc2ca42e4ae02af0a83b
Bi12Python <xovek11875@bitvoo.com>
2024-04-10 21:34:20 +0530

    Alerts, exception, screenshot

--- Deepesh/SeleniumCode/FunctionFramework/selenium_locators.py
+++ Deepesh/SeleniumCode/FunctionFramework/selenium_locators.py
@@ -1,6 +1,6 @@
 from selenium.webdriver.common.by import By
 
-first_name_locator = (By.XPATH, "(//input[@id='firstname'])[1]")
+first_name_locator = (By.XPATH, "(//12input[@id='firstname'])[1]")
 last_name_locator = (By.XPATH, "(//input[@id='firstname'])[2]")
 male_locator = (By.ID, "male")
 female_locator = (By.ID, "female")
