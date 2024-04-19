9f756c590b87f5d5f9b78f4daa7764eb54d50619
madhurirahane <madhuri.rahane66@gmail.com>
2024-04-19 14:01:56 +0530

    Amazon data added

--- madhuri/SeleniumCode/Projects/amazon/amazon_data.py
+++ madhuri/SeleniumCode/Projects/amazon/amazon_data.py
@@ -1,7 +1,7 @@
 create_account_page_title = "Amazon Registration"
 name = 'madhuri rahane'
 mobile_number = '9860138618'
-password = '29nikhil_m'
+password = 'Test@123'
 
 # login data
 login_title = 'Amazon Sign In'
