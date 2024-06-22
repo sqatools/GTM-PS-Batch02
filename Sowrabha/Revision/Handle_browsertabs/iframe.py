cd49d8e4b10f91c85e3e2c04cf4d945bd11e10ad
sowrabhaQA <sowrihr123@gmail.com>
2024-06-22 17:11:54 +0530

    iframe and action chain

--- Sowrabha/Revision/Handle_browsertabs/iframe.py
+++ Sowrabha/Revision/Handle_browsertabs/iframe.py
@@ -4,17 +4,19 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.common.alert import Alert
 
 driver=webdriver.Chrome()
 wait=WebDriverWait(driver,15,0.5)
-driver.get("https://www.globalsqa.com/demo-site/frames-and-windows")
+driver.get("https://www.globalsqa.com/demo-site/frames-and-windows/#iFrame")
 driver.maximize_window()
 
-iframe_element=wait.until(ec.visibility_of_element_located((By.ID,"iFrame")))
-iframe_element.click()
+
+iframe_element = wait.until(ec.visibility_of_element_located((By.NAME,"globalSqa")))
+driver.switch_to.frame(iframe_element)
 time.sleep(6)
 
-driver.switch_to.frame(iframe_element)
+
 search_element=wait.until(ec.visibility_of_element_located((By.ID,"s")))
 search_element.send_keys("selenium")
 search_element.click()
