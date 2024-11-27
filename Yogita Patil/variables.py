bb1851f7511dbcaab14068459f400fe18533d075
Yogitapy <jangaleyogita09@gmail.com>
2024-11-27 15:33:13 +0530

    adding data types program

---
+++ Yogita Patil/variables.py
@@ -0,0 +1,12 @@
+var1=10
+print(id(var1))
+
+#multiple variables holding same value so the memory address will be same for.
+
+var2=20
+var3=20
+print(id(var2))
+print(id(var3))
+
+#rules of defining the variables
+# Varibale name should not start with number
